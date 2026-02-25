8281e3c380d587910416ff6d4336af6ac612a1e9
edsf <ednaldo.dilorenzo@gmail.com>
2026-02-25 20:35:48 -0300

    Adição do banco de dados.

--- 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/controller/UsuarioController.java
+++ 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/controller/UsuarioController.java
@@ -1,27 +1,32 @@
 package br.edu.ifpb.sr.dac.demo.controller;
 
+import br.edu.ifpb.sr.dac.demo.dto.GetUsuariosRespDTO;
 import br.edu.ifpb.sr.dac.demo.dto.PostUsuarioDTO;
 import br.edu.ifpb.sr.dac.demo.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/usuarios")
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
 
     @PostMapping
     public ResponseEntity<Boolean> postUsuario(@RequestBody PostUsuarioDTO dto) {
-        System.out.println("Nome: " + dto.nome());
         this.usuarioService.save(dto);
         return ResponseEntity.created(URI.create("/1")).body(Boolean.TRUE);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetUsuariosRespDTO>> getAllUsuarios() {
+        return ResponseEntity.ok(this.usuarioService.listAll());
     }
 }
--- 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
+++ 202601/demo/src/main/java/br/edu/ifpb/sr/dac/demo/service/UsuarioServiceImpl.java
@@ -1,17 +1,23 @@
 package br.edu.ifpb.sr.dac.demo.service;
 
 import br.edu.ifpb.sr.dac.demo.dao.UsuarioDao;
+import br.edu.ifpb.sr.dac.demo.dto.GetUsuariosRespDTO;
 import br.edu.ifpb.sr.dac.demo.dto.PostUsuarioDTO;
 import br.edu.ifpb.sr.dac.demo.model.Usuario;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 
-    @Autowired
-    private UsuarioDao usuarioDao;
+    private final UsuarioDao usuarioDao;
+
+    public UsuarioServiceImpl(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
 
     @Override
     @Transactional
@@ -21,5 +27,14 @@ public class UsuarioServiceImpl implements UsuarioService {
         usuario.setSenha(dto.senha());
         usuario.setUsername(dto.username());
         this.usuarioDao.save(usuario);
+    }
+
+    @Override
+    public List<GetUsuariosRespDTO> listAll() {
+        return this.usuarioDao.
+                findAll().
+                stream().
+                map(usuario -> new GetUsuariosRespDTO(usuario.getId(), usuario.getNome(), usuario.getUsername())).
+                toList();
     }
 }
